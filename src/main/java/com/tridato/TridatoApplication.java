de7337a090a5ead415a6753a33a735169e43ab78
Daniela Beltran <ing.danibeltran@gmail.com>
2018-04-14 12:51:23 -0500

    Creación del proyecto

---
+++ src/main/java/com/tridato/TridatoApplication.java
@@ -0,0 +1,12 @@
+package com.tridato.tridato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TridatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TridatoApplication.class, args);
+	}
+}
