e24f9d1f39f2fe63784f71f1634493f592b28106
azurebird <white.camilo@gmail.com>
2018-04-15 13:32:49 -0500

    Comments added

--- tridato-eureka/src/main/java/com/tridato/eureka/Main.java
+++ tridato-eureka/src/main/java/com/tridato/eureka/Main.java
@@ -4,6 +4,9 @@ package com.tridato.eureka;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ *
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class Main {
