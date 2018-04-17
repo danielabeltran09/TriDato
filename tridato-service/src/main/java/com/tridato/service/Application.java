a44b57f198cc6bdce662365742bcefb2da112f0f
azurebird <white.camilo@gmail.com>
2018-04-16 22:00:03 -0500

    Configuration changed

--- tridato-service/src/main/java/com/tridato/service/Application.java
+++ tridato-service/src/main/java/com/tridato/service/Application.java
@@ -3,9 +3,9 @@ package com.tridato.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class Application {
 
