d4846e690156365e0d6c448bda8c8e40dd49ae8b
azurebird <white.camilo@gmail.com>
2018-04-17 08:17:50 -0500

    CO Police project started with ribbon

---
+++ tridato-co-police/src/main/java/com/tridato/service/Application.java
@@ -0,0 +1,32 @@
+package com.tridato.service;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@EnableEurekaClient
+@SpringBootApplication
+@RestController
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping(value = "/greeting")
+    public String greet() {
+
+        List<String> greetings = Arrays.asList("Hi there", "Greetings", "Salutations");
+        Random rand = new Random();
+
+        int randomNum = rand.nextInt(greetings.size());
+        return greetings.get(randomNum);
+    }
+}
