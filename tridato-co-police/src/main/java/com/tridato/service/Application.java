3ecac2b9ed3f7da0a20c35b444f609ad2390eaac
azurebird <white.camilo@gmail.com>
2018-04-18 22:19:06 -0500

    Zuul added

--- tridato-co-police/src/main/java/com/tridato/service/Application.java
+++ tridato-co-police/src/main/java/com/tridato/service/Application.java
@@ -13,20 +13,9 @@ import java.util.Random;
 
 @EnableEurekaClient
 @SpringBootApplication
-@RestController
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @RequestMapping(value = "/greeting")
-    public String greet() {
-
-        List<String> greetings = Arrays.asList("Hi there", "Greetings", "Salutations");
-        Random rand = new Random();
-
-        int randomNum = rand.nextInt(greetings.size());
-        return greetings.get(randomNum);
     }
 }
---
+++ tridato-co-police/src/main/java/com/tridato/service/ColombianPoliceService.java
@@ -0,0 +1,17 @@
+package com.tridato.service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/colombian-police")
+public class ColombianPoliceService {
+
+    @GetMapping("/health")
+    public String health() {
+
+        return "Colombian police is alive 2";
+    }
+}
+
