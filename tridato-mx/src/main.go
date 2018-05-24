d35affdf0ba08b76fca30dd61a969e9786f71902
Ignacio Gomez <i.gomez10@uniandes.edu.co>
2018-05-24 13:18:13 +0200

    Dockerfile for Curp validation with Readme

--- tridato-mx/src/main.go
+++ tridato-mx/src/main.go
@@ -1,22 +1,15 @@
 package main
 
-import "fmt"
 import "sipso"
 import "github.com/gin-gonic/gin"
 
-//import "encoding/json"
-
 func main() {
 	router := gin.Default()
-	//sipso := sipso.GetData("AACJ960403HNTLLN03")
-	//fmt.Println(sipso)
-	router.GET("/sipso/:curb", func(c *gin.Context) {
-		fmt.Println(c.Param("curb"))
-		response := sipso.GetData(c.Param("curb"))
-		//fmt.Println(response)
+	router.GET("/api/v1/curp/:curp", func(c *gin.Context) {
+		response := sipso.GetData(c.Param("curp"))
 		c.JSON(200, response)
 	})
 
-	router.Run(":8080")
+	router.Run(":3435")
 
 }
