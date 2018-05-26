607cdc77ad206322280524ea666a399de8db92f7
azurebird <white.camilo@gmail.com>
2018-05-25 22:08:10 -0500

    TRUD-1 Service implemented

--- td-mx-sipso/src/main/java/com/tridato/service/MexicoSipsoService.java
+++ td-mx-sipso/src/main/java/com/tridato/service/MexicoSipsoService.java
@@ -1,8 +1,15 @@
 package com.tridato.service;
 
-import org.jsoup.Connection;
+import com.tridato.service.api.SipsoApiResponse;
+import com.tridato.service.constants.SipsoSelector;
+import com.trudato.commons.api.ApiResponse;
+import org.apache.commons.lang.StringUtils;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +22,9 @@ import java.io.IOException;
 @RequestMapping("/mexican-sipso")
 public class MexicoSipsoService {
 
+    @Autowired
+    private Environment environment;
+
     @Value("${trudato.mx.sipso.url.index}")
     private String index;
 
@@ -23,9 +33,44 @@ public class MexicoSipsoService {
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/query/{curp}")
-    public String query(@PathVariable("curp") final String curp) throws IOException {
+    @GetMapping(path = "/query/{curp}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ApiResponse> query(@PathVariable("curp") final String queriedCurp) throws IOException {
 
-        final Connection.Response loginForm = Jsoup.connect(String.format(index, curp)).execute();
+        final Document curpDocument = Jsoup.connect(String.format(index, queriedCurp)).get();
+        return ResponseEntity.ok(new SipsoApiResponse()
+                .withCurp(getTextFromSelector(curpDocument, SipsoSelector.CURP))
+                .withLastName(getTextFromSelector(curpDocument, SipsoSelector.LAST_NAME))
+                .withSecondLastName(getTextFromSelector(curpDocument, SipsoSelector.SECOND_LAST_NAME))
+                .withName(getTextFromSelector(curpDocument, SipsoSelector.NAME))
+                .withGender(getTextFromSelector(curpDocument, SipsoSelector.GENDER))
+                .withBirthday(getTextFromSelector(curpDocument, SipsoSelector.BIRTHDAY))
+                .withBirthdayEntity(getTextFromSelector(curpDocument, SipsoSelector.BIRTHDAY_ENTITY)));
+    }
+
+    @GetMapping("/query/{curp}/name/{name}")
+    public ResponseEntity<ApiResponse> query(
+            @PathVariable("curp") final String queriedCurp,
+            @PathVariable("name") final String queriedName) throws IOException {
+
+        final Document curpDocument = Jsoup.connect(String.format(index, queriedCurp)).get();
+        final String name = curpDocument.select(environment.getProperty(SipsoSelector.NAME)).text();
+
+        if(!queriedName.equals(name)) {
+            return ResponseEntity.ok(new SipsoApiResponse());
+        }
+
+        return ResponseEntity.ok(new SipsoApiResponse()
+                .withCurp(getTextFromSelector(curpDocument, SipsoSelector.CURP))
+                .withLastName(getTextFromSelector(curpDocument, SipsoSelector.LAST_NAME))
+                .withSecondLastName(getTextFromSelector(curpDocument, SipsoSelector.SECOND_LAST_NAME))
+                .withName(getTextFromSelector(curpDocument, SipsoSelector.NAME))
+                .withGender(getTextFromSelector(curpDocument, SipsoSelector.GENDER))
+                .withBirthday(getTextFromSelector(curpDocument, SipsoSelector.BIRTHDAY))
+                .withBirthdayEntity(getTextFromSelector(curpDocument, SipsoSelector.BIRTHDAY_ENTITY)));
+    }
+
+    private String getTextFromSelector(final Document document, final String selector) {
+
+        return  document.select(environment.getProperty(selector)).text().trim();
     }
 }
