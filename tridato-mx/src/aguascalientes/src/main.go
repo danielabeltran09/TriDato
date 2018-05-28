e12101bf624ea33de8e5caaaf8a5bae0a5190296
Ignacio Gomez <i.gomez10@uniandes.edu.co>
2018-05-28 04:27:47 +0200

    Docker-compose ready for aguascalientes
    MYSQL database docker container
    Golang scraper docker container
    
    Golang scraper pipeline to dump data into mysql
    
    Docker-compose integration
    
    Readme

--- tridato-mx/src/aguascalientes/src/main.go
+++ tridato-mx/src/aguascalientes/src/main.go
@@ -1,60 +1,94 @@
 package main
 
-import "AreaID"
-import "fmt"
-import "Aguascalientes"
-import "strconv"
-import "sync"
+import (
+	"Aguascalientes"
+	"AreaID"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	"gopkg.in/cheggaaa/pb.v1"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
 
 func main() {
+	time.Sleep(time.Second * 30)
+	db, err := gorm.Open("mysql", "username:password@(172.19.0.2:3306)/dev?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		log.Println("ERROR CONNECTING DB ", err)
+		log.Fatal("Shutting Down")
+	}
+	db.Table("Aguascalientes")
+	defer db.Close()
+
 	availableAreas := checkAvailableAreas()
 	//var wg sync.WaitGroup
-	for i := 2018; i >= 2016; i-- { // YEARS
-		for j := 5; j <= 5; j++ { // MONTHS
-			for z := 1; z <= 12; z++ { // DAYS
-				//wg.Add(64)
-				for x := 0; x < len(availableAreas); x++ { // AVAILABLE GROUP AREAS(JUZGADOS)
-					pDate := strconv.Itoa(z) + "/" + strconv.Itoa(j) + "/" + strconv.Itoa(i)
-					//			func(x int, pDate string) {
-					if len(availableAreas[x]) > 0 {
-						for q := 0; q < len(availableAreas[x]); q++ { // AVAILABLE CATEGORIES INSIDE THE GROUP AREA
-							arr, err := Aguascalientes.GetData(x, availableAreas[x][q], pDate)
-							if err == nil {
-								for q := 0; q < len(arr); q++ {
-									fmt.Println(arr[q])
+	//wg.Add(64)
+	// AVAILABLE GROUP AREAS(JUZGADOS)
+	const timeLayout string = "02/01/2006"
+
+	var currentTime time.Time = time.Now()
+	currentTime = currentTime.AddDate(0, 0, -1)
+	currentTime = currentTime.AddDate(0, 0, -1)
+	lowerDateLimit, _ := time.Parse(timeLayout, "01/01/2017")
+	for ; currentTime.After(lowerDateLimit); currentTime = currentTime.AddDate(0, 0, -1) {
+
+		if currentTime.Weekday() == 0 || currentTime.Weekday() == 6 {
+			// Skip weekends
+			continue
+		}
+		// prepare go routines
+		var wg sync.WaitGroup
+		wg.Add(len(availableAreas))
+
+		pDate := currentTime.Format(timeLayout)
+		for x := 0; x < len(availableAreas); x++ {
+			func(x int, pDate string) {
+				if len(availableAreas[x]) > 0 {
+					// AVAILABLE CATEGORIES INSIDE THE GROUP AREA
+					for q := 0; q < len(availableAreas[x]); q++ {
+						arr, err := Aguascalientes.GetData(x, availableAreas[x][q], pDate)
+						if err == nil {
+							for q := 0; q < len(arr); q++ {
+								var de Aguascalientes.DataEntry
+								de = arr[q]
+								isNew := db.NewRecord(de)
+								if !isNew {
+									log.Printf("This entry already exist, %+v", de)
+								} else {
+									db.Create(&de)
 								}
-							} else {
-								//fmt.Printf("Error from Aguascalientes: %s \n ", err)
 							}
+						} else {
+							fmt.Printf("Error from Aguascalientes: %s \n ", err)
 						}
 					}
-					//	wg.Done()
-					//			}(x, pDate)
 				}
-			}
+				wg.Done()
+			}(x, pDate)
 		}
-		//wg.Wait()
-		fmt.Println("Finished")
+		wg.Wait()
 	}
 }
 
 func checkAvailableAreas() [][]int {
-	var wg sync.WaitGroup
+	log.Println("Retrieving corresponding areas: \n ")
+	bar := pb.StartNew(65)
 	var listAreas = make([][]int, 65)
-	wg.Add(64)
 	for i := 1; i < len(listAreas); i++ {
-		go func(i int) {
-			arr, err := AreaID.GetGrupoMateriasPorAreaID(i)
-			if err != nil {
-				//fmt.Printf("ERROR calling AreaID with %d, %s \n ", i, err)
-			} else {
-				listAreas[i] = arr
-				fmt.Println("ready ", i, arr)
-			}
-			wg.Done()
-		}(i)
+		arr, err := AreaID.GetGrupoMateriasPorAreaID(i)
+		if err != nil {
+			// log.Printf("ERROR calling AreaID with %d, %s \n ", i, err)
+			// fmt.Printf("[%v , []] ", i)
+		} else {
+			listAreas[i] = arr
+			// fmt.Printf("[%v , %v] ", i, arr)
+		}
+		bar.Increment()
 	}
-	wg.Wait()
-	fmt.Println("Finished Checking Available areas", listAreas)
+	bar.FinishPrint("Finished Checking Available Areas")
 	return listAreas
 }
